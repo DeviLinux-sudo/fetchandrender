c51a2097dea53e9797092bdd44c651659327c072
Alpeacha <eshkon.arohi1@gmail.com>
2022-11-04 16:28:43 +0530

    tried sequentical calling of API's

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
-function makeThreeApiCalls(){
-    fetch("https://jsonplaceholder.typicode.com/posts").then(response => {
-        return response.json();
-    }).then(data => {
-        console.log(data);
-    }).catch(error => {
-        console.log(error);
-    });
-    fetch("https://jsonplaceholder.typicode.com/todos").then(response => {
-        return response.json();
-    }).then(data => {
-        console.log(data);
-    }).catch(error => {
-        console.log(error);
-    });
-    fetch("https://jsonplaceholder.typicode.com/users").then(response => {
-        return response.json();
-    }).then(data => {
-        console.log(data);
-    }).catch(error => {
-        console.log(error);
-    });
+async function makeThreeApiCalls(){
+   const [postResponse, userResponse ]  = await Promise.all([
+    fetch('https://jsonplaceholder.typicode.com/posts'),
+    fetch('https://jsonplaceholder.typicode.com/users')
+   ]);
+
+   if (!postResponse.ok && userResponse.ok) {
+    const message = `An error has occured: ${postResponse.status || userResponse.status}`;
+    throw new Error(message);
+  }
+   
+   const post = await postResponse.json();
+   const users = await userResponse.json();
+   return [post,users];
 }
 
-makeThreeApiCalls();
+makeThreeApiCalls().then(([post,users]) => {
+    console.log(post);
+    console.log(users);
+}).catch(error => {
+error.message;
+});
