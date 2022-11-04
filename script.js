5c5159f608e116d0405536fe3ea943c81b563b85
Alpeacha <eshkon.arohi1@gmail.com>
2022-11-04 14:48:56 +0530

    added fetch calls

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ function makeThreeApiCalls(){
     }).catch(error => {
         console.log(error);
     });
+    fetch("https://jsonplaceholder.typicode.com/todos").then(response => {
+        return response.json();
+    }).then(data => {
+        console.log(data);
+    }).catch(error => {
+        console.log(error);
+    });
+    fetch("https://jsonplaceholder.typicode.com/users").then(response => {
+        return response.json();
+    }).then(data => {
+        console.log(data);
+    }).catch(error => {
+        console.log(error);
+    });
 }
 
 makeThreeApiCalls();
